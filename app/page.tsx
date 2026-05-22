53168e0223b6682ec6b3f0697299ec06eb1e72e5
Kaue <gkaue2005g@gmail.com>
2026-05-22 20:44:14 -0300

    Botão funcionando

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,15 @@
-export default function Home() {
-  return (
-    <main
-      style={{
-        background: "#090909",
-        color: "white",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        fontFamily: "Arial",
-      }}
-    >
-      <h1
-        style={{
-          fontSize: "80px",
-          color: "#7B2EFF",
-        }}
-      >
-        AsterFlix
-      </h1>
-
-      <p>Seu universo de entretenimento.</p>
-
-      <button
-        style={{
-          marginTop: "20px",
-          padding: "15px 30px",
-          background: "#7B2EFF",
-          border: "none",
-          borderRadius: "10px",
-          color: "white",
-          fontSize: "18px",
-        }}
-      >
-        Assistir Agora
-      </button>
-    </main>
-  );
-}
+<button
+  onClick={() => alert("Bem-vindo à AsterFlix 🔥")}
+  style={{
+    marginTop: "20px",
+    padding: "15px 30px",
+    background: "#7B2EFF",
+    border: "none",
+    borderRadius: "10px",
+    color: "white",
+    fontSize: "18px",
+    cursor: "pointer",
+  }}
+>
+  Assistir Agora
+</button>
